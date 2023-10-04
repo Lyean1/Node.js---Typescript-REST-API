50f943f5abd2732fbfe2afe1653537c9583e0534
Lyean1 <lyean1500@gmail.com>
2023-10-04 16:33:21 +0630

    update

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -7,7 +7,13 @@ export class UsersRoutes extends CommonRoutesConfig {
     }
 
     configureRoutes() {
-        // (we'll add the actual route configuration here next)
+        this.app.route(`/users`)
+        .get((req: express.Request, res: express.Response) => {
+            res.status(200).send(`List of users`);
+        })
+        .post((req: express.Request, res: express.Response) => {
+            res.status(200).send(`Post to users`);
+        })
         return this.app;
     }
 
