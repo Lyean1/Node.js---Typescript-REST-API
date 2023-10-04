ef91e29a7fb086bc704f4d2dfc16c848912c8c58
Lyean1 <lyean1500@gmail.com>
2023-10-04 16:29:43 +0630

    update

--- common/common.routes.config.ts
+++ common/common.routes.config.ts
@@ -1,4 +1,4 @@
-import express from 'express';
-export abstract class CommonRoutesConfig {
-    app: express.Application;
-    name: string;
+constructor(app: express.Application, name: string) {
+    this.app = app;
+    this.name = name;
+    this.configureRoutes();
