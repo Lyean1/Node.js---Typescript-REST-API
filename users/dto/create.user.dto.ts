0faaa41c13d5a3653fed8abf795411580f97616b
Lyean1 <lyean1500@gmail.com>
2023-10-18 23:25:37 +0630

    update

--- users/dto/create.user.dto.ts
+++ users/dto/create.user.dto.ts
@@ -6,3 +6,12 @@ export interface CreateUserDto {
     lastName?: string;
     permissionLevel?: number;
 }
+
+export interface PutUserDto {
+    id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    permissionLevel: number;
+}
