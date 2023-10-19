480cdeeea818f14f03e4765d5fd04964a87873b1
Lyean1 <lyean1500@gmail.com>
2023-10-19 18:34:04 +0630

    update

--- users/daos/user.dao.ts
+++ users/daos/user.dao.ts
@@ -7,3 +7,12 @@ import debug from 'debug';
 
 const log: debug.IDebugger = debug('app:in-memory-dao');
 
+class UsersDao {
+    users: Array<CreateUserDto> = [];
+
+    constructor() {
+        log('Created new instance of UsersDao');
+    }
+}
+
+export default new UsersDao();
