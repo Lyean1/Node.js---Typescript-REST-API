5a553f22c772675445f30e8eac913f78e4205a97
Lyean1 <lyean1500@gmail.com>
2023-10-20 23:19:31 +0630

    update

--- users/daos/user.dao.ts
+++ users/daos/user.dao.ts
@@ -67,4 +67,11 @@ async patchUserById(userId: string, user: PatchUserDto) {
             return null;
         }
     }
-    
+    export interface CRUD {
+        list: (limit: number, page: number) => Promise<any>;
+        create: (resource: any) => Promise<any>;
+        putById: (id: string, resource: any) => Promise<string>;
+        readById: (id: string) => Promise<any>;
+        deleteById: (id: string) => Promise<string>;
+        patchById: (id: string, resource: any) => Promise<string>;
+    }
